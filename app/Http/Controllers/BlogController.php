2e4174906bcb42503f85a8c14f859cb336cd890d
SpacePeant <seanmatthew0915@gmail.com>
2025-05-16 15:08:03 +0700

    benerin header blog

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -9,57 +9,44 @@ use Illuminate\Support\Facades\DB;
 
 class BlogController extends Controller
 {
-    // BlogController.php
-    public function loadMore(Request $request)
-    {
-    $offset = $request->input('offset', 0);
-    $limit = 3; // Load 3 at a time
-
-    $blogs = Blog::skip($offset)->take($limit)->get();
-
-    return response()->json($blogs);
-    }
-
-
-    // public function showBlogPage()
+    // // BlogController.php
+    // public function loadMore(Request $request)
     // {
-    //     $carouselImages = DB::table('blog_image')->get(); 
+    // $offset = $request->input('offset', 0);
+    // $limit = 3; // Load 3 at a time
 
-    //     return view('blog', compact('carouselImages')); 
+    // $blogs = Blog::skip($offset)->take($limit)->get();
+
+    // return response()->json($blogs);
     // }
 
-    // public function showArticlePage()
-    // {
-    //     $articles = DB::table('article_image')->get(); 
-
-    //     return view('blog', compact('articles')); 
-    // }
-
-    public function showBlogPage()
+     public function showBlogPage()
     {
         $articles = DB::table('article_image')->get(); 
-        $carouselImages = DB::table('blog_image')->get(); // Adjust table name if needed
+        $carouselImages = DB::table('blog_image')->get(); // Adjust this if needed
 
         return view('blog', compact('articles', 'carouselImages'));
     }
 
+    // Handle AJAX "load more" requests
     public function loadMoreBlogs(Request $request)
-{
-    $offset = $request->input('offset', 0);
-    $blogs = DB::table('article_image')
-                ->offset($offset)
-                ->limit(6)
-                ->get()
-                ->map(function ($blog) {
-                    return [
-                        'title' => $blog->title,
-                        'excerpt' => $blog->description,
-                        'image_url' => asset('image/image_article/' . $blog->filename),
-                    ];
-                });
+    {
+        $offset = $request->input('offset', 0);
 
-    return response()->json($blogs);
-}
+        $blogs = DB::table('article_image')
+                    ->offset($offset)
+                    ->limit(6)
+                    ->get()
+                    ->map(function ($blog) {
+                        return [
+                            'title' => $blog->title,
+                            'excerpt' => $blog->description,
+                            'image_url' => asset('image/image_article/' . $blog->filename),
+                        ];
+                    });
+
+        return response()->json($blogs);
+    }
 
 
 }
