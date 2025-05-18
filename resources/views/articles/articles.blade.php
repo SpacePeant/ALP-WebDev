4a36d8bc95d134312a419a1d6bf5f0aa7ab3b7a4
SpacePeant <seanmatthew0915@gmail.com>
2025-05-18 15:08:39 +0700

    kasi isi article, article click yang di load more masi rusak

--- resources/views/articles/articles.blade.php
+++ resources/views/articles/articles.blade.php
@@ -6,7 +6,8 @@
 <main style="max-width: 800px; margin: auto; padding: 20px;">
     <h1>{{ $article->title }}</h1>
     <img src="{{ asset('image/image_article/' . $article->filename) }}" alt="{{ $article->title }}" style="width: 100%; border-radius: 8px; margin-bottom: 20px;">
-    <p>{{ $article->description }}</p>
+    <h4>{{ $article->description }}</h4>
+    <p>{{ $article->article }}</p>
 </main>
 @endsection
 @extends('articles.layout')
