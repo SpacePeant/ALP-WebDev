72cbb4ce9c02c52fe166170d4d2ffe0f1c4edeb1
BearBranddddd <bfrederick@student.ciputra.ac.id>
2025-05-26 20:31:15 +0700

    debikecil

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,10 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->text('address')->nullable();
+            $table->string('phone_number', 15)->nullable();
             $table->string('password');
+            $table->string('role',10)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
