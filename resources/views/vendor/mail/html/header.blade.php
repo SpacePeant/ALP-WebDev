ae3cfd7d6dc2a16b2a9cfe10e5cacb79587e06bf
BearBranddddd <bfrederick@student.ciputra.ac.id>
2025-06-06 00:43:59 +0700

    koko

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{ asset('image/logo2.png') }}" class="logo" alt="MyApp Logo" style="height: 50px;">
+            <img src="{{ asset('image/logo2.png') }}" class="logo" alt="MyApp Logo">
         </a>
     </td>
 </tr>
