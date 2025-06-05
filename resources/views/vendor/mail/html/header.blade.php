7849528aedef0cda14dc39762359b4b4bf3d27e5
BearBranddddd <bfrederick@student.ciputra.ac.id>
2025-06-06 01:23:52 +0700

    lplp

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{ asset('image/logo2.png') }}" class="logo" alt="MyApp Logo" style="width: 50%;">
+            <img src="{{ asset('image/logo2.png') }}" class="logo" alt="MyApp Logo" style="width: 30%; height: 30%;">
         </a>
     </td>
 </tr>
