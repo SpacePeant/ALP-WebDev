1d408458ab95e4d50127dda6536b6575a090c77f
BearBranddddd <bfrederick@student.ciputra.ac.id>
2025-06-08 15:06:40 +0700

    lplp

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Socialite;
+use Laravel\Socialite\Facades\Socialite;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
